743e2bc0c58e962a7ade32bc484dd8e8b34b6b59
Sudhindra Deshpande <sudhiinlook@gmail.com>
2026-02-21 15:33:24 +0530

    insert voice bot

--- ai_backend/query.py
+++ ai_backend/query.py
@@ -11,7 +11,16 @@ import shutil
 from tempfile import NamedTemporaryFile
 from pydantic import BaseModel,Field
 import os
+import pyttsx3
+from fastapi.concurrency import run_in_threadpool
+from fastapi.staticfiles import StaticFiles
+import re
 app=FastAPI()
+
+AUDIO_DIR = "audio_files"
+os.makedirs(AUDIO_DIR, exist_ok=True)
+
+app.mount("/audio", StaticFiles(directory=AUDIO_DIR), name="audio")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -19,10 +28,15 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
+def generate_audio(text: str, filename: str)->None:
+    engine = pyttsx3.init()
+    engine.save_to_file(text, filename)
+    engine.runAndWait()
+    engine.stop() 
 class UserInput(BaseModel):
     ques:str=Field(...,description="question to be asked")
 client=OpenAI(
-    api_key="AIzaSyD58dn7TLCfdGm2uEkVOrsuTk_Xe_ot1aE",
+    api_key="AIzaSyCj0Fq9r5ulrCDePRh-QXsXjgbf0vR20s8",
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
 text_splitter=RecursiveCharacterTextSplitter(
@@ -33,6 +47,7 @@ embeddings = HuggingFaceEmbeddings(
     model_name="BAAI/bge-small-en-v1.5",
     encode_kwargs={"normalize_embeddings": True}
 )
+qdrant_client = QdrantClient(url="http://localhost:6333")
 
 @app.post("/uploadfile/")
 async def upload(file:UploadFile=File(...),ques: str = Form(...)):
@@ -40,16 +55,20 @@ async def upload(file:UploadFile=File(...),ques: str = Form(...)):
     with NamedTemporaryFile(delete=False,suffix=".pdf") as tmp:
         shutil.copyfileobj(file.file,tmp)
         temp_path=tmp.name
-    loader=PyPDFLoader(file_path=temp_path) 
-    docs=loader.load()
-    chunks=text_splitter.split_documents(documents=docs)
-    vector_store=QdrantVectorStore.from_documents(
-    documents=chunks,
-    embedding=embeddings,
-    url="http://localhost:6333",
-    collection_name=name
-    )
-    qdrant_client = QdrantClient(url="http://localhost:6333")
+
+    if not qdrant_client.collection_exists(name):
+    
+        loader = PyPDFLoader(temp_path)
+        docs = loader.load()
+        chunks = text_splitter.split_documents(docs)
+
+        QdrantVectorStore.from_documents(
+        documents=chunks,
+        embedding=embeddings,
+        url="http://localhost:6333",
+        collection_name=name
+       )
+    
     vector_db=QdrantVectorStore(
     client=qdrant_client,
     collection_name=name,
@@ -78,7 +97,17 @@ async def upload(file:UploadFile=File(...),ques: str = Form(...)):
         {"role":"user","content":user_query}
     ]
     )
-    return {"answer":response.choices[0].message.content}
+    resp=response.choices[0].message.content
+    
+    safe_name = re.sub(r"[^a-zA-Z0-9_-]", "_", os.path.splitext(name)[0])
+    audio_filename = f"{safe_name}.wav"
+    audio_filepath = os.path.join(AUDIO_DIR, audio_filename)
+    await run_in_threadpool(generate_audio, resp, audio_filepath)
+    
+    return {
+        "answer": resp,
+        "audio_url": f"http://127.0.0.1:8000/audio/{audio_filename}"        # full URL path for the client
+    }
     
 if __name__ == "__main__":
     import uvicorn
